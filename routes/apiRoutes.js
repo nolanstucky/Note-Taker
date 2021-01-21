ea0cb2ea4dfd8199c5bfa308d0130037fe7efcb4
Nolan Stucky <nolan@stucky.net>
2021-01-20 20:43:32 -0800

    added deletefile function and also trying to fix 404 error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,11 @@
+//npm to write and read files
 const fs = require('fs');
+//npm that assigns a unique id when called upon
 const uuid4 = require('uuid4');
 const notes = [];
-
+//reads the db file when /notes loads
 readDbFile();
+//app requests for index.js
 module.exports = function(app){
     app.get("/api/notes", (req, res) => {
         res.json(notes);
@@ -14,14 +17,19 @@ module.exports = function(app){
         writeDbFile(notes);
         res.json(true);
     })
-}
 
+    app.delete("/api/index/:id", (req, res) =>{
+        deleteDbFile(req);
+        res.send('200');
+    })
+}
+//writes the db.json file with argument data
 function writeDbFile(noteInfo){
     fs.writeFile('db/db.json', JSON.stringify(noteInfo, null, 2), (err) => {
         if (err) throw err;
     })
 }
-
+//reads the already written db.json file
 function readDbFile(){
     fs.readFile('db/db.json', (err, data) =>{
         if (err) throw err;
@@ -31,4 +39,19 @@ function readDbFile(){
         });
     })
 }
+//deletes the object with specefic id 
+function deleteDbFile(noteInfo){
+    fs.readFile('db/db.json',(err, data) => {
+        if (err) throw err;
+        let deleteNote = noteInfo.params.id;
+        let jsonNote = JSON.parse(data);
+        console.log(jsonNote);
+        jsonNote.forEach((item, i) => {
+            if (item.id.includes(deleteNote)){
+                jsonNote.splice(i,1)
+            }
+        });
 
+        writeDbFile(jsonNote);
+    })
+}

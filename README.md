18d502d3e66a827b5e880a3d7c1e77a7605ee6c1
Nolan Stucky <nolan@stucky.net>
2021-01-20 21:28:44 -0800

    added comments to code and finished readme file.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Note-Taker
+
+## Description:
+An application hosted with heroku that allows the user to start taking notes by clicking on a "Get Started" button. The user is transfered to the note page which they are able to write notes with a title and text for a single note. They are also able to save the note to the server. Additionally they can get rid of any note by clicking on a red trash can deleting the note permently. 
+
+## Demonstration:
+![Alt text](./public/assets/img/note-taker2.jpg?raw=true "Title")
+![Alt text](./public/assets/img/note-taker1.jpg?raw=true "Title")
+## Heroku Link:
+https://ns-note-taker.herokuapp.com/notes
+
+## Contributing:
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](https://www.contributor-covenant.org/version/2/0/code_of_conduct/)
+
+## Questions:
+* Email: <nolan@stucky.net>
+* GitHub Profile: [nolanstucky](https://github.com/nolanstucky)
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-
+//require all npm packages needed
 const express = require("express");
 const fs = require('fs');
 const path = require('path');
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+//Sets up inital port
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//using apiRoutes.js for api requests
 require("./routes/apiRoutes")(app);
 
-
+//html get requests for main page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+//html get request for note page
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
-
+//using cs and js files
 app.use(express.static(path.join(__dirname, "public")))
 
-
+//listen request for the app to function
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
